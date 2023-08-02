a9b936fb6bf1647fdcb920a694ddf302a031aa59
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-08-02 23:06:02 +0300

    unit tests

--- tests/course.test.ts
+++ tests/course.test.ts
@@ -1,15 +1,26 @@
 import { createCourse } from "../src/service/course.service";
 import * as repository from '../src/repository/course.reposutory';
 
-describe("createCourse function", () => {
-    test("Успешно", async () => {
-        const functionInRepository = jest.spyOn(repository, 'createCourseDB');
-        functionInRepository.mockResolvedValue([{ id: 1, course: 'javascript' }]);
-        const result = await createCourse('javascript');
 
-        expect(functionInRepository).toHaveBeenCalled();
+describe("createCourse", () => {
+    test("Успешно", async () => {
+        const mock = jest.spyOn(repository, 'createCourseDB');
+        mock.mockResolvedValue([{ id: 1, course: 'javascript' }]);
+        const result = await createCourse('javascript'); 
+
+        expect(mock).toHaveBeenCalled()
+        expect(result).toEqual([{ id: 1, course: 'javascript' }])
     })
 })
+// describe("createCourse", () => {
+//     test("Успешно", async () => {
+//         const functionInRepository = jest.spyOn(repository, 'createCourseDB');
+//         functionInRepository.mockResolvedValue([{ id: 1, course: 'javascript' }]);
+//         const result = await createCourse('javascript');
+
+//         expect(functionInRepository).toHaveBeenCalled();
+//     })
+// })
 
 // describe("getAllCourse function", () => {
 //     test("Успешно", async () => {
@@ -38,6 +49,19 @@ describe("updateCourseByIDDB function", () => {
         const functionInRepository = jest.spyOn(repository, 'updateCourseByIDDB');
         functionInRepository.mockResolvedValue([{ id: 1, course: 'typescript' }]);
         const result = await repository.updateCourseByIDDB("typescript", 1);
+        expect(functionInRepository).toHaveBeenCalled()
+        expect(result).toEqual([{ id: 1, course: 'typescript' }])
+        expect(result.length).toBeGreaterThan(0); 
+
+    })
+})
+
+
+describe("deleteCourseByIDDB function", () => {
+    test("Успешно", async () => {
+        const functionInRepository = jest.spyOn(repository, 'deleteCourseByIDDB');
+        functionInRepository.mockResolvedValue([{ id: 1, course: 'typescript' }]);
+        const result = await repository.deleteCourseByIDDB(1);
         expect(functionInRepository).toHaveBeenCalled()
         expect(result).toEqual([{ id: 1, course: 'typescript' }])
         expect(result.length).toBeGreaterThan(0); 
